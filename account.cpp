37b24a4a2cac1d4eed57c530db25465d59cf3d60
pavander <pavander@umich.edu>
2023-11-07 13:33:34 -0500

    wtf

---
+++ account.cpp
@@ -0,0 +1,15 @@
+// Project Identifier: 292F24D17A4455C1B5133EDD8C7CEAA0C9570A98
+
+#include "account.h"
+
+using namespace std;
+
+class Account {
+public:
+    Account();
+
+private:
+    uint32_t balance;
+    
+
+};
---
+++ bank.cpp
@@ -0,0 +1,51 @@
+// Project Identifier: 292F24D17A4455C1B5133EDD8C7CEAA0C9570A98
+
+#include <algorithm>
+#include <getopt.h>
+#include <iostream>
+#include "account.h"
+
+using namespace std;
+
+int main (int argc, char* argv[]) {
+    //getoptlong
+    //getoptlong time
+    ios_base::sync_with_stdio(false);
+
+    //cout << "good morning \n";
+
+    static struct option long_options[] = {
+    {"help",        no_argument,        NULL,  'h'},
+    {"verbose",     no_argument,        NULL,  'v'},
+    {"file",        no_argument,        NULL,  'f'},
+    {0,             0,                  NULL,  0, }
+    };
+    int option_index = 0;
+
+    bool verbose = false;
+    bool file = false;
+    string filename;
+
+    int c = getopt_long(argc, argv, "hvf:", long_options, &option_index);
+    while (c != -1) {
+        switch(c) {
+            case 'h' :
+                cout << "help message";
+                exit(0);
+                break;
+            case 'v' :
+                verbose = true;
+                break;
+            case 'f' :
+                file = true;
+                filename = optarg;
+                break;
+        }
+        c = getopt_long(argc, argv, "hvf:", long_options, &option_index);
+    }
+
+    if (!file) {
+        cout << "registrations not specified\n";
+        return 1;
+    }
+}
